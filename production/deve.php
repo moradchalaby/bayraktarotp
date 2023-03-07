6d2c23855a4672223408cfa1b58eeb9055a4d8c6
vcv <vv>
2023-03-07 08:59:27 +0300

    as

--- production/deve.php
+++ production/deve.php
@@ -1,9 +1,9 @@
 <?php
 include '../netting/baglan.php';
 
-/* ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL); */
+error_reporting(E_ALL);
 include 'header.php';
 
 //BELİRLİ VERİYİ SEÇME İŞLEMİ
